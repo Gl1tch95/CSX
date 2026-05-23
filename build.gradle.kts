93d238f927e81c51bbdec1a4c7ad0f0075f5bc05
Gl1tch95 <chrishemswthor@yahoo.com>
2026-05-23 15:09:35 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,7 +35,7 @@ subprojects {
     apply(plugin = "com.lagradost.cloudstream3.gradle")
 
     cloudstream {
-        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/SaurabhKaperwan/CSX")
+        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/Gl1tch95/CSX")
         authors = listOf("megix")
     }
 
